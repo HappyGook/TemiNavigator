52beebea9debe274ef276ad255d7108787cfc118
oleg.radzhabov <oleg.radzhabov@stu.haw-kiel.de>
2026-09-24 15:22:01 +0200

    add temi object implementing robot controller interface

--- app/src/main/java/com/example/teminavigator/TemiRobot.kt
+++ app/src/main/java/com/example/teminavigator/TemiRobot.kt
@@ -1,23 +1,65 @@
 package com.example.teminavigator
-import android.util.Log
+import com.example.teminavigator.domain.RobotController
+import com.example.teminavigator.domain.RobotEvent
 import com.robotemi.sdk.Robot
 import com.robotemi.sdk.TtsRequest
+import com.robotemi.sdk.listeners.OnGoToLocationStatusChangedListener
 import com.robotemi.sdk.listeners.OnRobotReadyListener
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-object TemiRobot: OnRobotReadyListener {
-    val robot: Robot
-        get()=Robot.getInstance()
-    var ready = false
+object TemiRobot: RobotController, OnRobotReadyListener, OnGoToLocationStatusChangedListener {
+    private val robot: Robot get()=Robot.getInstance()
 
-    init{
+    // private constantly changing version
+    private var _ready = MutableStateFlow(false)
+
+    // public on demand var
+    var ready : StateFlow<Boolean> = _ready
+
+    private var _events = MutableSharedFlow<RobotEvent>(extraBufferCapacity = 10)
+    override var events = _events
+
+    override val availableLocations: List<String>
+        get() = robot.locations
+
+
+    // funcs to subscribe / unsubscribe the robot to events from sdk
+    fun attach(){
         robot.addOnRobotReadyListener(this)
+        robot.addOnGoToLocationStatusChangedListener(this)
+    }
+
+    fun detach(){
+        robot.removeOnRobotReadyListener(this)
+        robot.removeOnGoToLocationStatusChangedListener(this)
+    }
+
+    override fun goTo(locationId: String) = robot.goTo(locationId)
+    override fun stop() = robot.stopMovement()
+    override fun speak(text: String){
+        robot.speak(TtsRequest.create(text,false))
+    }
+    // TODO: find out the exact home location name (Raum 2.72?)
+    override fun goHome() = robot.goTo("home?")
+
+    override fun onGoToLocationStatusChanged(
+        location: String,
+        status: String,
+        descriptionId: Int,
+        description: String
+    ) {
+        TODO("Not yet implemented")
     }
 
     override fun onRobotReady(isReady: Boolean){
-        ready = isReady
+        _ready.value = isReady
+        /*
         robot.speak(TtsRequest.create("HALLLOOOOO!!!!!"))
         Log.i("Locations", robot.locations.toString())
         robot.goTo("home")
         robot.setCurrentGoToSpeed(1.5f)
+         */
     }
 }
