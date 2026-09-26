742f51e62e16cf0cf16169c3ee58e9bc496568df
oleg.radzhabov <oleg.radzhabov@stu.haw-kiel.de>
2026-09-26 19:17:35 +0200

    visual improvements of the home screen and interactivity

--- app/src/main/java/com/example/teminavigator/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/teminavigator/ui/screens/HomeScreen.kt
@@ -1,8 +1,15 @@
 package com.example.teminavigator.ui.screens
+import android.annotation.SuppressLint
 import android.graphics.BitmapFactory
+import android.graphics.MaskFilter
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.hoverable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,12 +18,16 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -26,6 +37,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
@@ -34,6 +46,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -42,9 +55,13 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.teminavigator.domain.Destination
 import com.example.teminavigator.ui.theme.TemiNavigatorTheme
 
@@ -115,51 +132,93 @@ val mockDestinations = listOf(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text("Temi Navigator")
-                }
-            ) }
-    ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Row{
-                Surface(
-                    modifier = Modifier.fillMaxWidth(0.3f).fillMaxHeight()
-                ){
-                    LocationsList(mockDestinations)
-                }
-                Surface(
-                    color = Color.Blue,
-                    modifier = Modifier.fillMaxWidth().fillMaxHeight()
-                ){
-                    InteractiveMap()
-                }
+    Scaffold{ innerPadding ->
+        Row(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(12.dp)
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Surface(
+                shape = RoundedCornerShape(16.dp),
+                color = MaterialTheme.colorScheme.surfaceContainer,
+                modifier = Modifier
+                    .fillMaxWidth(0.3f)
+                    .fillMaxHeight()
+            ) {
+                LocationsList(
+                    mockDestinations
+                )
             }
-
+            Surface(
+                shape = RoundedCornerShape(16.dp),
+                color = MaterialTheme.colorScheme.tertiary,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            ) {
+                InteractiveMap(
+                    modifier = Modifier.padding(8.dp),
+                    onClick = {print("TODO")} // TODO: Navigation
+                )
+            }
         }
     }
 }
 
 @Composable
 fun LocationsList(locationList: List<Destination>){
-        LazyColumn() {
+        LazyColumn(Modifier.padding(vertical = 5.dp)) {
             items(locationList){location -> PossibleLocation(location=location)}
         }
 }
 
+@SuppressLint("RememberInComposition")
 @Composable
-fun PossibleLocation(location : Destination){
-    Row(Modifier.fillMaxWidth(0.9f)){
+fun PossibleLocation(
+    location : Destination,
+    onClick: (Destination) -> Unit = {}
+){
+    val interactionSource = remember { MutableInteractionSource() }
+    val isHovered by interactionSource.collectIsHoveredAsState()
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(vertical = 6.dp, horizontal = 5.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .border(
+                width=1.dp,
+                color= MaterialTheme.colorScheme.tertiary,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .background(
+                if (isHovered) MaterialTheme.colorScheme.secondaryContainer
+                else MaterialTheme.colorScheme.surface
+            )
+            .hoverable(interactionSource = interactionSource)
+            .clickable(
+                interactionSource = interactionSource,
+                indication = ripple(),
+                onClick = {onClick(location)}
+                )
+            .padding(16.dp)
+    ){
         Column{
-            Text(location.displayName)
+            Text(
+                text=location.displayName,
+                modifier = Modifier.fillMaxWidth(),
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onSurface
+            )
             LazyRow {
-                items(location.aliases){alias -> Text("$alias ")}
+                items(location.aliases){
+                    alias ->
+                    Text(
+                        text = "$alias ",
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )}
             }
         }
     }
@@ -167,6 +226,7 @@ fun PossibleLocation(location : Destination){
 
 @Composable
 fun InteractiveMap(
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
     assetFileName: String = "uni_map.png" // image from assets
 ) {
@@ -215,7 +275,21 @@ fun InteractiveMap(
                 )
         )
 
-        // TODO: add pins on the map
+            // TODO: add pins on the map
+
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+            FloatingActionButton(
+                onClick = onClick
+            ){
+                Icon(Icons.Default.Settings, contentDescription = "Settings")
+            }
+        }
 
         // Zoom controls
         Column(
